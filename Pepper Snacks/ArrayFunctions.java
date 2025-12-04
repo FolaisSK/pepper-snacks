2b460bdd3553e7e01f6013ef3731edbfca8dcd26
Fola <folajimibabslawal@gmail.com>
2025-12-04 15:57:55 +0100

    upd

--- Pepper Snacks/ArrayFunctions.java
+++ Pepper Snacks/ArrayFunctions.java
@@ -24,7 +24,17 @@ public class ArrayFunctions{
 
 	public static int[] oddPositions(int[] arr){
 
-		int newLength = arr.length / 2;
+		int newLength = 0;
+
+		if(arr.length % 2 != 0){
+			
+			newLength = (arr.length + 1) / 2;
+
+		}else{
+
+			newLength = arr.length / 2;
+
+		}
 		int[] oddDisplay = new int[newLength]; 
 
 
@@ -118,6 +128,94 @@ public class ArrayFunctions{
 		return true;
 
 	}
+
+
+
+
+	public static int[] reverse(int[] arr){
+
+		int[] reversed = new int[arr.length];
+
+		int index = 0;
+		for(int count = arr.length -1; count >= 0; count--){
+
+			reversed[index] = arr[count];
+			index++;
+		}
+
+		return reversed;
+
+	}
+
+
+
+	public static int[] joinArray(int[] arrOne, int[] arrTwo){
+
+		int newLength = arrOne.length + arrTwo.length;
+		int[] combined = new int[newLength];
+
+		int index = 0;
+		int counter = 0;
+		for(int count = 0; count <= combined.length; count++){
+
+//			combined[index] = arrOne[count];
+
+			if(count <= arrOne.length){
+
+				combined[index] = arrOne[count];
+				index++;
+
+			}else{
+
+				combined[index] = arrTwo[counter];
+				counter++;
+			
+			}
+		
+
+
+
+		}
+
+		return combined;
+
+
+	}
+
+
+
+
+	public static int[] arrayCombined(int[] arrOne, int[] arrTwo){
+
+		int newLength = arrOne.length + arrTwo.length;
+		int [] output = new int [newLength]; 
+	
+		int counter = 0;
+		for(int count = 0; count<= newLength; count++){
+
+			if(count % 2 != 0){
+
+				output[] = arrOne[counter];
+				counter++;
+
+			}else{
+
+				output[] = arrTwo[counter];
+				counter++
+
+			}
+
+		}
+
+		return output;
+
+
+	}
+
+
+
+
+
 
 
 
